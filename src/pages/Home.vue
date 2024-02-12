deab2c5dfcb59495f4c332a3f5ca8fe0fa90caf0
ksh03003 <74225015+kwakchaewon@users.noreply.github.com>
2024-02-12 14:08:55 +0900

    api 연동 및 테스트 데이터 출력

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -14,12 +14,15 @@
                 </div>
             </div>
         </section>
-
         <div class="album py-5 bg-light">
             <div class="container">
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                    <div class="col" v-for="i in 12" :key="i">
-                        <Card/>
+<!--                    <div class="col" v-for="i in 12" :key="i">-->
+
+                    <!-- items 라는 배열 안에 String 데이터 타입이 들어감 -->
+                    <div class="col" v-for="(item, idx) in state.items" :key="idx">
+<!--                        {{item}}-->
+                        <Card :item="item" />
                     </div>
                 </div>
             </div>
@@ -28,14 +31,26 @@
 </template>
 
 <script>
-    import Card from "@/components/Card";
+import axios from "axios";
+import {reactive} from "vue";
+import Card from "@/components/Card";
 
-    export default {
-        name: 'Home',
-        components: {
-        Card
-      }
+export default {
+    name: 'Home',
+    components: {Card},
+
+    setup(){
+        const state = reactive({
+            items:[]
+        })
+
+        axios.get("api/items").then(({data}) =>{
+            state.items = data
+        })
+
+        return {state};
     }
+}
 </script>
 
 <style scoped>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+    devServer: {
+        proxy: {
+
+            // api 주소 조회시 target 주소로 우회
+            '/api': {
+                target: 'http://localhost:8082',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/': ''
+                }
+            }
+        }
+    }
+}
+
+//const { defineConfig } = require('@vue/cli-service')
+//module.exports = defineConfig({
+//  transpileDependencies: true
+//})
