eabc0f3878f4bbc97ed82d3aa8989cdb08f5a62d
ksh03003 <74225015+kwakchaewon@users.noreply.github.com>
2024-02-15 19:03:05 +0900

    item 목록 화면에 표기

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="card shadow-sm">
-        <svg class="bd-placeholder-img card-img-top" width="100%" height="225"
-             xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
-             preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
-            <rect width="100%" height="100%" fill="#55595c"/>
-            <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-        </svg>
-
+<!--        <img :src="item.imgPath"/>-->
+<!--        <svg class="bd-placeholder-img card-img-top" width="100%" height="225"-->
+<!--             xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"-->
+<!--             preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>-->
+<!--            <rect width="100%" height="100%" fill="#55595c"/>-->
+<!--            <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>-->
+<!--        </svg>-->
+        <img :src="item.imgPath"/>
         <div class="card-body">
-            <p class="card-text">{{item}}</p>
+            <p class="card-text">{{item.name}}</p>
+            <p class="card-text">{{item.price}}</p>
+            <p class="card-text">{{item.discountPer}}</p>
             <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                     <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
@@ -23,7 +26,7 @@
     export default {
       name: 'Card',
       props:{
-        item: String
+        item: Object
       }
     }
 </script>
